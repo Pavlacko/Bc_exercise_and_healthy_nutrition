95a1d31489fd2b728ad1dd85363df535f73f54d2
Marek Pavlac <pavlac@stud.uniza.sk>
2026-01-21 18:51:40 +0100

    add tutorial UI + calculators

---
+++ Controllers/CalculatorsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bc_exercise_and_healthy_nutrition.Controllers
+{
+    public class CalculatorsController : Controller
+    {
+        public IActionResult BMI()
+        {
+            return View();
+        }
+
+        public IActionResult BMR()
+        {
+            return View();
+        }
+
+        public IActionResult Calories()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EducationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bc_exercise_and_healthy_nutrition.Controllers
+{
+    public class EducationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calories()
+        {
+            return View();
+        }
+
+        public IActionResult Macros()
+        {
+            return View();
+        }
+
+        public IActionResult Tips()
+        {
+            return View();
+        }
+    }
+}
