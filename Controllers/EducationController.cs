cbd02bd26a7ffb1507209501f6cf937ec8a59440
Marek Pavlac <pavlac@stud.uniza.sk>
2026-03-13 17:45:27 +0100

    Add exercise database page with videos and grouped muscle sections

--- Controllers/EducationController.cs
+++ Controllers/EducationController.cs
@@ -1,16 +1,33 @@
-﻿using Bc_exercise_and_healthy_nutrition.Filters;
+﻿using Bc_exercise_and_healthy_nutrition.Data;
+using Bc_exercise_and_healthy_nutrition.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bc_exercise_and_healthy_nutrition.Controllers
 {
     [RequireLogin]
     public class EducationController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public EducationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-    
+        public async Task<IActionResult> Exercises()
+        {
+            var exercises = await _context.Exercises
+                .OrderBy(e => e.MuscleGroup)
+                .ThenBy(e => e.Name)
+                .ToListAsync();
+
+            return View(exercises);
+        }
     }
 }
